ca84e144066f80f76dc2f0294a322535c2cb3bfb
geekyfaahad <77067677+geekyfaahad@users.noreply.github.com>
2021-12-07 16:18:49 -0800

    Add files via upload

---
+++ pypacks.sh
@@ -0,0 +1,25 @@
+pip install pip
+pip install --upgrade pip
+python -m pip install --upgrade pip
+pip install colorama
+pip install beautifulsoup4
+pip install numpy
+pip install pandas
+pip install matplotlib
+pip install seaborn
+pip install scikit-learn
+pip install setuptools
+pip install urllib3
+pip install pytest
+pip install pywhatkit
+#pip install face-recognition
+pip install selenium
+pip install certifi
+pip install pyttsx3
+pip install emoji
+pip install wikipedia
+pip install figlet
+pip install faunadb
+pip install antigravity
+pip install mysql-connector-python
+exit
