cc32c4faa510421fa80eb64034e9f392ce6556b4
tantutuu <95428454+tantutuu@users.noreply.github.com>
2022-07-08 02:20:42 +0530

    Update pypacks.sh
    we can add list packages by just adding name of the package without adding pip install to every <package>

--- pypacks.sh
+++ pypacks.sh
@@ -1,1 +1,4 @@
+pip install pip
+pip install --upgrade pip
+python -m pip install --upgrade pip
 pip3 install colorama requests numpy pandas dhooks matplotlib seaborn scikit-learn setuptools urllib3 tk pytest pywhatkit #face-recognition selenium opencv-python certifi pyttsx3 emoji wikipedia figlet faunadb antigravity mysql-connector-python pylunch pyinstaller py2exe bs4 lxml PyPDF2 urllib3
